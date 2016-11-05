4ab1f1356ca295c6e619b2c6ad0ea67fa07fe0bf
Randall Leeds <randall@bleeds.info>
2016-11-05 12:32:34 -0700

    increase emacs font size

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -34,4 +34,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :height 60 :width normal :foundry "unknown" :family "Source Code Pro")))))
+ '(default ((t (:slant normal :weight normal :height 70 :width normal :foundry "unknown" :family "Source Code Pro")))))
