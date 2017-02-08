49c1a96ecad8b3b086a1489cc0e1b9f754a41011
Randall Leeds <randall@bleeds.info>
2017-02-08 15:58:17 -0800

    re-enable yapf

--- .emacs.d/personal/tilgovi.el
+++ .emacs.d/personal/tilgovi.el
@@ -49,9 +49,9 @@
          ("C-<" . mc/mark-previous-like-this)
          ("C-c C-<" . mc/mark-all-like-this)))
 
-;; (use-package py-yapf
-;;   :config
-;;   (add-hook 'python-mode-hook 'py-yapf-enable-on-save))
+(use-package py-yapf
+  :config
+  (add-hook 'python-mode-hook 'py-yapf-enable-on-save))
 
 (use-package rust-mode
   :config
