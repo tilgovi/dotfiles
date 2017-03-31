76d141535471aacfec67fd8a259330a824b706c7
Randall Leeds <randall@bleeds.info>
2017-03-30 22:29:55 -0700

    upgrade to emacs 25

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -7,9 +7,14 @@
    (quote
     ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(eldoc-idle-delay 0)
+ '(package-selected-packages
+   (quote
+    (zop-to-char zencoding-mode zenburn-theme yari yaml-mode whitespace-cleanup-mode which-key web-mode volatile-highlights virtualenvwrapper use-package undo-tree toml-mode theme-changer terraform-mode solarized-theme smex smartrep smartparens smart-mode-line rustfmt ruby-tools rainbow-mode rainbow-delimiters racer python pylint py-yapf paredit ov operate-on-number nginx-mode multiple-cursors move-text markdown-mode magit less-css-mode json-rpc json-mode jedi inf-ruby imenu-anywhere idomenu ido-ubiquitous highlight helm-pydoc helm-projectile helm-flymake helm-descbinds helm-css-scss helm-ag haskell-mode guru-mode groovy-mode grizzl gradle-mode gotest google-c-style god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck-rust flycheck-mypy flx-ido expand-region erlang elpy elisp-slime-nav ein editorconfig-core editorconfig easy-kill dockerfile-mode discover-my-major diff-hl dash-functional csv-mode crux company-quickhelp company-jedi company-go company-anaconda color-theme-solarized coffee-mode cargo browse-kill-ring birds-of-paradise-plus-theme beacon anzu ace-window ace-jump-mode ace-jump-buffer)))
  '(python-shell-extra-pythonpaths (quote ("~/.local/venvs")))
- '(save-place t nil (saveplace))
+ '(replace-char-fold t)
+ '(save-place-mode t nil (saveplace))
  '(scroll-bar-mode nil)
+ '(search-default-mode (quote char-fold-to-regexp))
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
