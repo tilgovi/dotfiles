30af3780720664f7d46cf6a56e88508b5128e076
Randall Leeds <randall@remix.com>
2017-07-17 17:25:36 -0700

    Prettify symbols in Emacs

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -8,6 +8,7 @@
     ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(eldoc-idle-delay 0)
  '(flycheck-disabled-checkers (quote (javascript-jshint)))
+ '(global-prettify-symbols-mode t)
  '(js-indent-level 2)
  '(js-switch-indent-offset 2)
  '(js2-bounce-indent-p t)
@@ -17,6 +18,7 @@
    (quote
     (ace-window add-node-modules-path auto-virtualenv cargo chruby color-theme-solarized company company-anaconda company-flow company-go company-jedi company-quickhelp company-tern diminish dockerfile-mode editorconfig elpy erlang flow-mode flycheck flycheck-mypy flycheck-rust geiser gitconfig-mode gitignore-mode google-c-style gotest helm helm-core import-js jedi js2-mode json-mode less-css-mode magit magit-popup markdown-mode multiple-cursors nginx-mode py-yapf pylint python racer rainbow-delimiters rainbow-mode rjsx-mode robe rust-mode rustfmt smart-mode-line smartparens smex solarized-theme tern terraform-mode theme-changer toml-mode undo-tree use-package web-mode whitespace-cleanup-mode yaml-mode yasnippet zencoding-mode)))
  '(prelude-whitespace nil)
+ '(prettify-symbols-unprettify-at-point (quote right-edge))
  '(python-shell-extra-pythonpaths (quote ("~/.local/venvs")))
  '(replace-char-fold t)
  '(save-place-mode t nil (saveplace))
