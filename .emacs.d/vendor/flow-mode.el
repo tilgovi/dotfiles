f3647583b652296a1ee07686a1dc75a8bfdb63f8
Randall Leeds <randall@remix.com>
2018-03-25 22:42:44 -0700

    Create flow-mode.el as a vendored package

---
+++ .emacs.d/vendor/flow-mode.el
@@ -0,0 +1,24 @@
+;;; flow-mode.el --- Flow support for Emacs          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Randall Leeds
+
+;; Author: Randall Leeds <randall@bleeds.info>
+;; Keywords: javascript, flow, languages
+;; Package-Requires: (typescript-mode)
+
+;;; Commentary:
+
+;; This package derives a new mode from typescript-mode for editing JavaScript
+;; with Flow types.
+
+;;; Code:
+
+(require 'typescript-mode)
+
+(define-derived-mode flow-mode typescript-mode "Flow"
+  "JavaScript with Flow type checking")
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . flow-mode))
+
+(provide 'flow-mode)
+;;; flow-mode.el ends here
