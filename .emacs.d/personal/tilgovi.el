4580aa405486c69738b1e5ca2345156b4ef58aac
Randall Leeds <randall@remix.com>
2018-03-25 22:43:10 -0700

    Refactor personal Emacs init
    - Use flow-mode instead of typescript-mode
    
    - Do not chain flow to eslint, expect eslint to have the flow plugin
    
    - Use :hook, :mode, :init, :requires, :interpreter, etc... make the
      whole use-package situation a lot cleaner
    
    - Recursively find eslint and flow

--- .emacs.d/personal/tilgovi.el
+++ .emacs.d/personal/tilgovi.el
@@ -10,21 +10,6 @@
 (when window-system (toggle-frame-fullscreen))
 ;;(setq frame-title-format (list "%b - " invocation-name "@" system-name))
 
-;; Use JSX always for JavaScript files
-(add-to-list 'auto-mode-alist '("\\.m?jsx?\\'" . js2-jsx-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
-
-;; https://emacs.stackexchange.com/a/27609
-(defun my/use-eslint-from-node-modules ()
-  (let ((root (locate-dominating-file
-               (or (buffer-file-name) default-directory)
-               (lambda (dir)
-                 (let ((eslint (expand-file-name "node_modules/eslint/bin/eslint.js" dir)))
-                   (and eslint (file-executable-p eslint)))))))
-    (when root
-      (let ((eslint (expand-file-name "node_modules/eslint/bin/eslint.js" root)))
-        (setq-local flycheck-javascript-eslint-executable eslint)))))
-
 ;; This definition and the hook below auto-balance and -center windows.
 (defun center-window (window)
   "Tile and center buffers in WINDOW at 80 columns."
@@ -49,73 +34,91 @@
    (walk-windows 'center-window 'f)))
 
 (use-package add-node-modules-path
-  :config
-  (eval-after-load 'js2-mode
-    '(add-hook 'js2-jsx-mode-hook #'add-node-modules-path)))
+  :hook ((flow-mode . add-node-modules-path) (js2-mode . add-node-modules-path)))
 
 (use-package auto-virtualenv
-  :config
-  (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv))
+  :hook (python-mode . auto-virtualenv-set-virtualenv))
 
 (use-package chruby
-  :config
-  (add-hook 'ruby-mode-hook 'chruby-use-corresponding))
+  :hook (ruby-mode . chruby-use-corresponding))
 
 (use-package company)
 
 (use-package company-flow
+  :init
+  (add-to-list 'company-backends 'company-flow)
   :config
-  (eval-after-load 'company
-    '(add-to-list 'company-backends 'company-flow)))
+  (add-to-list 'company-flow-modes 'flow-mode))
 
 (use-package company-quickhelp
+  :functions company-quickhelp-mode
   :config
   (company-quickhelp-mode 1))
 
 (use-package company-terraform
-  :config
-  (eval-after-load 'company
-    '(add-to-list 'company-backends 'company-terraform)))
+  :requires company
+  :init
+  (add-to-list 'company-backends 'company-terraform))
 
 (use-package company-tern
-  :config
-  (eval-after-load 'company
-    '(add-to-list 'company-backends 'company-tern t)))
+  :requires company
+  :init
+  (add-to-list 'company-backends 'company-tern t))
 
 (use-package elpy
+  :functions elpy-enable
   :config
   (elpy-enable))
 
-(use-package flycheck
-  :config
-  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
-  (flycheck-add-next-checker 'python-flake8 'python-pylint)
-  (global-flycheck-mode))
-
 (use-package editorconfig
-  :config
-  (add-hook
-   'editorconfig-custom-hooks
-   (lambda (props) (whitespace-mode))))
+  :hook (editor-config-custom-hooks . (lambda (props) (whitespace-mode))))
 
 (use-package import-js)
 
 (use-package flow-minor-mode
+  :hook (flow-mode . flow-minor-enable-automatically))
+
+(use-package flow-mode
+  :mode "\\.m?jsx?\\'"
+  :interpreter "node")
+
+(use-package flycheck
+  :functions
+  (flycheck-add-mode flycheck-add-next-checker global-flycheck-mode)
+  :init
+  ;; https://emacs.stackexchange.com/a/27609
+  (defun my/use-eslint-from-node-modules ()
+    (let ((root (locate-dominating-file
+                 (or (buffer-file-name) default-directory)
+                 (lambda (dir)
+                   (let ((eslint (expand-file-name "node_modules/.bin/eslint" dir)))
+                     (and eslint (file-executable-p eslint)))))))
+      (when root
+        (let ((eslint (expand-file-name "node_modules/.bin/eslint" root)))
+          (setq-local flycheck-javascript-eslint-executable eslint)))))
   :config
-  (add-hook 'js2-jsx-mode-hook 'flow-minor-mode))
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+  (flycheck-add-mode 'javascript-eslint 'flow-mode)
+  (flycheck-add-next-checker 'python-flake8 'python-pylint)
+  (global-flycheck-mode))
 
 (use-package flycheck-flow
+  :requires flycheck
+  :init
+  ;; https://emacs.stackexchange.com/a/27609
+  (defun my/use-flow-from-node-modules ()
+    (let ((root (locate-dominating-file
+                 (or (buffer-file-name) default-directory)
+                 (lambda (dir)
+                   (let ((flow (expand-file-name "node_modules/.bin/flow" dir)))
+                     (and flow (file-executable-p flow)))))))
+      (when root
+        (let ((flow (expand-file-name "node_modules/.bin/flow" root)))
+          (setq-local flycheck-javascript-flow-executable flow)))))
   :config
-  (eval-after-load 'flow-minor-mode
-    '(advice-add
-      'flycheck-flow--predicate
-      :filter-return
-      (lambda (r)
-        "Only enable flow checkers on flow files"
-        (and r (flow-minor-tag-present-p)))))
-  (flycheck-add-next-checker 'javascript-eslint 'javascript-flow)
-  (flycheck-add-next-checker 'javascript-flow
-                             'javascript-flow-coverage))
+  (add-hook 'flycheck-mode-hook #'my/use-flow-from-node-modules)
+  (flycheck-add-mode 'javascript-flow'flow-mode)
+  (flycheck-add-mode 'javascript-flow-coverage 'flow-mode))
 
 (use-package multiple-cursors
   :bind (("C->" . mc/mark-next-like-this)
@@ -127,86 +130,51 @@
   (add-hook 'python-mode-hook 'py-yapf-enable-on-save))
 
 (use-package robe
+  :requires (chruby company)
+  :hook (ruby-mode . robe-mode)
   :config
-  (eval-after-load 'chruby
-    '(defadvice inf-ruby-console-auto
-         (before activate-rvm-for-robe activate)
-       (chruby-use-corresponding)))
-  (eval-after-load 'company
-    '(add-to-list 'company-backends 'company-robe t))
-  (add-hook 'ruby-mode-hook 'robe-mode))
+  (defadvice inf-ruby-console-auto
+      (before activate-rvm-for-robe activate) (chruby-use-corresponding))
+  (add-to-list 'company-backends 'company-robe t))
 
 (use-package rust-mode
-  :config
-  (setq-default rust-format-on-save t)
-  (use-package racer
-    :config
-    (add-hook 'rust-mode-hook 'racer-mode)))
+  :init
+  (setq-default rust-format-on-save t))
+
+(use-package racer
+  :requires rust-mode
+  :hook (rust-mode . racer-mode))
 
 (use-package smartparens
+  :functions smartparens-global-mode
   :config
   (smartparens-global-mode))
 
 (use-package terraform-mode
-  :config
-  (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode))
+  :hook (terraform-mode . terraform-format-on-save-mode))
 
 (use-package tern
-  :config
-  (setq tern-command (append tern-command '("--no-port-file")))
-  (add-hook 'js2-jsx-mode-hook (lambda () (tern-mode t))))
+  :hook ((flow-mode . tern-mode) (js2-mode . tern-mode))
+  :functions tern-mode
+  :init
+  (setq tern-command (append tern-command '("--no-port-file"))))
 
 (use-package theme-changer
-  :config
+  :functions change-theme
+  :init
   ;; Day / Night themes
   (setq calendar-location-name "Oakland, CA")
   (setq calendar-latitude 37.80)
   (setq calendar-longitude -122.27)
+  :config
   (change-theme 'solarized-light 'solarized-dark))
 
 (use-package toml-mode)
 
-(use-package typescript-mode
-  :config
-  ;; Derive a flow-mode from typescript-mode
-  (define-derived-mode flow-mode typescript-mode "Flow"
-    "JavaScript with Flow type checking")
-
-  ;; Use flow-mode for all kinds of JavaScript files
-  (add-to-list 'auto-mode-alist '("\\.m?jsx?\\'" . flow-mode))
-
-  ;; Use node as the interpreter for flow-mode
-  (add-to-list 'interpreter-mode-alist '("node" . flow-mode))
-
-  ;; Add node modules path to the default path under flow-mode.
-  (eval-after-load 'add-node-modules-path
-    '(add-hook 'flow-mode-hook #'add-node-modules-path))
-
-  ;; Enable company-flow to autocomplete for flow-mode.
-  (eval-after-load 'company-flow
-    '(add-to-list 'company-flow-modes 'flow-mode))
-
-  ;; Enable flow-minor-mode to get the type at point echoed.
-  (eval-after-load 'flow-minor-mode
-    '(add-hook 'flow-mode-hook 'flow-minor-mode))
-
-  ;; Enable flycheck with eslint for flow-mode.
-  (eval-after-load 'flycheck
-    '(flycheck-add-mode 'javascript-eslint 'flow-mode))
-
-  ;; Chain flow and flow-coverage flycheck to eslint.
-  (eval-after-load 'flycheck-flow
-    '(progn
-       (flycheck-add-mode 'javascript-flow' flow-mode)
-       (flycheck-add-mode 'javascript-flow-coverage' flow-mode)))
-
-  ;; Enable tern-mode when in flow-mode.
-  (eval-after-load 'tern-mode
-    '(add-hook 'flow-mode-hook (lambda () (tern-mode t)))))
-
 (use-package yasnippet)
 
 (use-package whitespace-cleanup-mode
+  :functions global-whitespace-cleanup-mode
   :config
   (global-whitespace-cleanup-mode))
 
