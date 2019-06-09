d85500024c471edb721dcdf4a057945a4ebb52db
Randall Leeds <randall@bleeds.info>
2019-06-08 23:48:50 -0700

    Enable auto-save-visited-mode in custom

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-visited-mode t)
  '(auto-virtualenv-dir "~/.local/share/virtualenvs")
  '(clang-format-style "Chromium")
  '(column-number-mode t)
