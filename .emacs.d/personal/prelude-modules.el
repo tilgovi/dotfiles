a4bd501f0bd07a00563bad91d308968d180039e4
Randall Leeds <randall@bleeds.info>
2019-12-26 11:59:55 -0800

    Harmonize prelude-modules.el with defaults

--- .emacs.d/personal/prelude-modules.el
+++ .emacs.d/personal/prelude-modules.el
@@ -1,17 +1,17 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
-;; (require 'prelude-erc) ;; Emacs IRC client
+;; Emacs IRC client
+(require 'prelude-erc)
 ;; (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
 (require 'prelude-ivy) ;; A mighty modern alternative to ido
 ;; (require 'prelude-helm) ;; Interface for narrowing and search
 ;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
-;; (require 'prelude-mediawiki)
 ;; (require 'prelude-evil)
 
 ;;; Programming languages support
-;; (require 'prelude-c)
+(require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
@@ -24,8 +24,9 @@
 (require 'prelude-js)
 ;; (require 'prelude-latex)
 (require 'prelude-lisp)
+(require 'prelude-lsp)
 ;; (require 'prelude-ocaml)
-;; (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 ;; (require 'prelude-perl)
 (require 'prelude-python)
 (require 'prelude-ruby)
@@ -33,7 +34,8 @@
 ;; (require 'prelude-scala)
 ;; (require 'prelude-scheme)
 (require 'prelude-shell)
-(require 'prelude-scss)
-(require 'prelude-web) ;; Emacs mode for web templates
-;; (require 'prelude-xml)
+;; (require 'prelude-scss)
+(require 'prelude-ts)
+;; (require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-xml)
 (require 'prelude-yaml)
