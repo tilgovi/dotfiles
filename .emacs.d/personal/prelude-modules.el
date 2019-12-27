082ccd63aa1ecec0e6911550650dc18e7bcc5a28
Randall Leeds <randall@bleeds.info>
2019-12-26 23:19:10 -0800

    Disable prelude-ts.el

--- .emacs.d/personal/prelude-modules.el
+++ .emacs.d/personal/prelude-modules.el
@@ -35,7 +35,7 @@
 ;; (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
-(require 'prelude-ts)
+;; (require 'prelude-ts)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 (require 'prelude-yaml)
