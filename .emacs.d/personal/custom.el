82f833e95dafe2468658b0be4301e9ee5f9cc897
Randall Leeds <randall@bleeds.info>
2020-05-12 11:05:38 -0700

    Use python3 for elpy

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -19,6 +19,7 @@
  '(elpy-modules
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
+ '(elpy-rpc-python-command "python3")
  '(fill-column 80)
  '(flycheck-checker-error-threshold 1000)
  '(flycheck-clang-language-standard "c++2a")
