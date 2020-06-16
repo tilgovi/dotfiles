6fb1dd261978817787c98c8f9bcc913eb2100f67
Randall Leeds <randall@bleeds.info>
2020-06-16 16:16:02 -0700

    Enable vterm-kill-buffer-on-exit

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -70,6 +70,7 @@
  '(typescript-auto-indent-flag nil)
  '(typescript-indent-level 2)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(vterm-kill-buffer-on-exit t)
  '(web-mode-attr-indent-offset 2)
  '(web-mode-code-indent-offset 2)
  '(web-mode-css-indent-offset 2)
