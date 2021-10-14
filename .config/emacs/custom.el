123dda3edbaadf2e76b294a75275c9d9a6c30e8f
Randall Leeds <randall@bleeds.info>
2021-10-14 10:50:51 -0700

    Make Terraform resource names easier to read in Emacs

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -119,4 +119,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(terraform--resource-name-face ((t (:inherit font-lock-variable-name-face)))))
