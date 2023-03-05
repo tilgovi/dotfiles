4200d915ccc59696d3dabcc41c10d8b65584721b
Randall Leeds <randall@bleeds.info>
2023-03-05 09:58:26 -0800

    Remove customized eldoc face

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -142,7 +142,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eldoc-box-body ((t (:family "sans-serif"))))
  '(fixed-pitch ((t (:family "Iosevka Term"))))
  '(terraform--resource-name-face ((t (:inherit font-lock-variable-name-face)))))
 
