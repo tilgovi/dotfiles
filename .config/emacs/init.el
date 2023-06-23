9f9aac865c242014eedbe2a13830606c9da4a6dd
Randall Leeds <randall@bleeds.info>
2023-06-23 17:55:26 -0400

    Use Iosevka Term Extended for monospace fonts

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -124,7 +124,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Iosevka Term"))))
+ '(fixed-pitch ((t (:width expanded :family "Iosevka Term"))))
  '(terraform--resource-name-face ((t (:inherit font-lock-variable-name-face))))
  '(variable-pitch ((t (:inherit default)))))
 
