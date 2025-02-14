49943d79be749c62b7e60b4892933e6f9673958e
Randall Leeds <randall@bleeds.info>
2025-02-14 09:36:24 -0800

    Remove mise shims mode

--- .oh-my-zsh/custom/mise.zsh
+++ .oh-my-zsh/custom/mise.zsh
@@ -2,8 +2,5 @@ if [[ -z "$MISE_DATA_DIR" ]]; then
   export MISE_DATA_DIR="${XDG_DATA_HOME:-${HOME}/.local/share}/mise"
 fi
 
-path=("$MISE_DATA_DIR/shims" $path)
-typeset -U path
-
 export MISE_NODE_COREPACK=true
 export MISE_NODE_DEFAULT_PACKAGES_FILE="${XDG_CONFIG_HOME:-${HOME}/.config}/default-npm-packages"
